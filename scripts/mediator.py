28f5879bbbd970b69b714aab252cae5bdcf32c8f
Snowtin18 <winston.doss@gmail.com>
2023-12-08 14:00:28 +0530

    idk i did a lot of changes

--- scripts/mediator.py
+++ scripts/mediator.py
@@ -12,23 +12,36 @@ from geometry_msgs.msg import Pose,PoseArray
 from nav_msgs.msg import Path
 
 
+cam_poses=[]
 
-
-def callback(msg):
-    poses=[]
+def cam_callback(msg):
 
     for i in msg.poses:
-        pose=[(i.position.x-320),-(i.position.y-240)]
-        poses.append(pose)
+        cam_pose=[(i.position.x),(i.position.y)]
+        cam_poses.append(cam_pose)
     
-    print(poses)
+    print(cam_poses)
+
+def lidar_callback(msg):
+    lidar_poses=[]
+
+    for i in msg.lidar_poses:
+        lidar_pose=[(i.position.x),(i.position.y)]
+        lidar_poses.append(lidar_pose)
+
+    #convert to cam pose
+    
+    print(lidar_poses)
+
+
 
 def main():
     global camposepub
     rospy.init_node('MediatorNode')
-    pose_sub = rospy.Subscriber('/detection_topic', PoseArray, callback)
 
-    #camposepub=rospy.Publisher('/camposeArray',PoseArray,queue_size=20)
+    cam_pose_sub = rospy.Subscriber('/CamDetections', PoseArray, cam_callback)
+    lidar_pose_sub = rospy.Subscriber('/LidarDetections', PoseArray, lidar_callback)
+
 
     rospy.spin()
 
